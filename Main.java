ed3e2a7311ef2cc7b58bd551f68b0105c3447930
BT_1N3_27 <leannec8@nycstudents.net>
2025-05-22 09:29:06 -0400

    Added user interaction example

--- Main.java
+++ Main.java
@@ -3,18 +3,28 @@
 //if you finish an assignment you can remove it from the list
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        ToDo test = new ToDo();
-        test.add("Math", "5-16-25");
-        test.add("CSA", "5-17-25");
-        test.remove("CSA");
-        test.add("AP LANG", "6-21-25");
-        test.add("CSA", "6-21-25");
-        System.out.println(test.printList());
-        System.out.println(test.getAssignments("6-21-25"));
+        //ToDo test = new ToDo();
+        //test.add("Math", "5-16-25");
+        //test.add("CSA", "5-17-25");
+        //test.remove("CSA");
+        //test.add("AP LANG", "6-21-25");
+        //test.add("CSA", "6-21-25");
+        //System.out.println(test.printList());
+        //System.out.println(test.getAssignments("6-21-25"));
+
+        //use scanner to interact with user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println("Hello, " + name + "! You are " + age + " years old!");
+        scanner.close();
     }
 }
